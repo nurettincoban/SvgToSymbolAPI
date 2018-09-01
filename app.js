8ccd3205aac9ca57a12b5a1568018cc7c58bce14
Nurettin Çoban <cobannurettin@gmail.com>
2018-09-02 01:16:38 +0300

    added endpoint to check root directory.

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
+app.get("/", (req, res)=>{
+    res.json({ message: 'Server is working' });
+});
+
 app.use('/api',svgAPI);
 
 app.listen(port, ()=>{
